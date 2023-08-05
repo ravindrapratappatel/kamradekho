d49a627dc8c86fead96f1369532119c4477d6e58
RAVINDRA PRATAP PATEL <ravindra955440@gmail.com>
2023-08-05 11:15:03 +0530

    added node.js.yml

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const path = require("path");
+app.use(express.static(path.join(__dirname, 'kamradekho/build')));
 var corsOptions = {
   origin: "http://localhost:8081"
 };
