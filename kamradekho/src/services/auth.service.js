11d61f214b8c2267e3ec9ab4b12324925171c9bc
RAVINDRA PRATAP PATEL <ravindra955440@gmail.com>
2023-08-06 20:10:14 +0530

    server fix

--- kamradekho/src/services/auth.service.js
+++ kamradekho/src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 //const API_URL = "http://localhost:5000/api/auth/";
-const API_URL = 'http://139.59.40.65:5000/api/data/';
+const API_URL = 'http://139.59.40.65:5000/api/auth/';
 class AuthService {
   login(username, password) {
     return axios
